523b3cd14fe10fb8e6eb10c5466c90eea890c82c
Rich Mcveigh <richmcveigh1@gmail.com>
2023-08-16 11:22:18 +0100

    removes console log in utils function

--- utils.js
+++ utils.js
@@ -4,7 +4,6 @@ const db = require("./db/connection");
 exports.checkExists = async (table, column, value) => {
     const queryStr = format('SELECT * FROM %I WHERE %I = $1;', table, column);
     const dbOutput = await db.query(queryStr, [value]);
-  console.log(dbOutput.rows)
     if (dbOutput.rows.length === 0) {
       return Promise.reject({ status: 404, msg: 'Not found' });
     }
