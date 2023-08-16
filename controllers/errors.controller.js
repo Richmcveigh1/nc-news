23cc5b5785fd21f0b9ae1f7e565a0534271ca0e3
Rich Mcveigh <richmcveigh1@gmail.com>
2023-08-16 10:45:05 +0100

    removes console.log from error controller

--- controllers/errors.controller.js
+++ controllers/errors.controller.js
@@ -6,7 +6,6 @@ exports.handle400s = (err, _, res, next) => {
 
 exports.handle404s = (err, _, res, next) => {
     if (err.code === '22003') {
-        console.log("in the handle 404s")
         res.status(404).send({msg: "Not found"})
     }
     else next(err)
