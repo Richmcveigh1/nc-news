d21ac00afc70385633cf3c653088e11fa1306563
Rich Mcveigh <richmcveigh1@gmail.com>
2023-08-17 15:14:38 +0100

    added users model and users controller. Not working yet. Pushing as I was working in main by accident

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const {
   postCommentWithArticleID,
   deleteComment
 } = require("./controllers/comments.controller");
+const { getAllUsers } = require('./controllers/users.controller')
 const {
   handle400s,
   handle404s,
@@ -35,6 +36,8 @@ app.post("/api/articles/:article_id/comments", postCommentWithArticleID);
 app.patch("/api/articles/:article_id", patchArticleWithVotes);
 
 app.delete("/api/comments/:comment_id", deleteComment)
+
+app.get("/api/users", getAllUsers)
 
 app.use((_, res) => {
   res.status(404).send({ msg: "Not found" });
---
+++ controllers/users.controller.js
@@ -0,0 +1,12 @@
+const { retrieveAllUsers } = require("../models/users.model")
+
+exports.getAllUsers = async (req, res, next) => {
+
+    try {
+        const users = await retrieveAllUsers
+        const allUsers = users.rows
+        res.status(200).send(allUsers)
+    } catch (err) {
+        next (err)
+    }
+}
