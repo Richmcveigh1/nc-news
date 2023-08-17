9b859a44548f845a231fc046c87944c097c22a7f
Rich Mcveigh <richmcveigh1@gmail.com>
2023-08-17 17:02:27 +0100

    refactors comments model to use try...catch

--- models/comments.model.js
+++ models/comments.model.js
@@ -1,43 +1,56 @@
 const db = require("../db/connection");
 
-exports.selectAllCommentsFromArticleID = (article_id) => {
-  return db.query(
-    `
-      SELECT
-      comment_id,
-      votes,
-      created_at,
-      author,
-      body,
-      article_id
-      FROM comments
-      WHERE article_id = $1
-      ORDER BY created_at DESC
-      `,
-    [article_id]
-  );
+exports.selectAllCommentsFromArticleID = async (article_id) => {
+  try {
+    const result = await db.query(
+      `
+        SELECT
+        comment_id,
+        votes,
+        created_at,
+        author,
+        body,
+        article_id
+        FROM comments
+        WHERE article_id = $1
+        ORDER BY created_at DESC
+        `,
+      [article_id]
+    );
+    return result;
+  } catch (err) {
+    throw err;
+  }
 };
 
 exports.addComment = (article_id, username, body) => {
-    return db.query(`
+  return db
+    .query(
+      `
     INSERT INTO comments
     (author, body, article_id)
     VALUES
     ($1, $2, $3)
     RETURNING *`,
-    [username, body, article_id]
-    ).then(( { rows }  ) => {
-        return rows[0]
-    })
-}
+      [username, body, article_id]
+    )
+    .then(({ rows }) => {
+      return rows[0];
+    });
+};
 
-exports.removeComment = (comment_id) => {
-    return db.query(`
-    DELETE FROM comments
-    WHERE comment_id = $1
-    RETURNING *
-    `,
-    [comment_id]).then (({ rows }) => {
-        return rows
-    })
-}
+exports.removeComment = async (comment_id) => {
+  try {
+    const result = await db.query(
+      `
+        DELETE FROM comments
+        WHERE comment_id = $1
+        RETURNING *
+        `,
+      [comment_id]
+    );
+    return result.rows;
+  } catch (err) {
+    throw err;
+  }
+};
