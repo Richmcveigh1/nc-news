0c2bb4c480f4019557c66f84e35bbfec49ccef4c
Rich Mcveigh <richmcveigh1@gmail.com>
2023-09-04 09:28:49 +0100

    adds CORS

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const { getTopics } = require("./controllers/topics.controller");
@@ -10,14 +11,16 @@ const {
 const {
   getAllCommentsForArticleFromID,
   postCommentWithArticleID,
-  deleteComment
+  deleteComment,
 } = require("./controllers/comments.controller");
-const { getAllUsers } = require('./controllers/users.controller')
+const { getAllUsers } = require("./controllers/users.controller");
 const {
   handle400s,
   handle404s,
   customErrorHandler,
 } = require("./controllers/errors.controller");
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -35,9 +38,9 @@ app.post("/api/articles/:article_id/comments", postCommentWithArticleID);
 
 app.patch("/api/articles/:article_id", patchArticleWithVotes);
 
-app.delete("/api/comments/:comment_id", deleteComment)
+app.delete("/api/comments/:comment_id", deleteComment);
 
-app.get("/api/users", getAllUsers)
+app.get("/api/users", getAllUsers);
 
 app.use((_, res) => {
   res.status(404).send({ msg: "Not found" });
